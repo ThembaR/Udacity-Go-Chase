4b6cc125b08532b400e5f7bde7eec4951cfa5e43
ThembaR <84722070+ThembaR@users.noreply.github.com>
2023-05-11 11:18:20 +0200

    Update process_image.cpp

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -22,34 +22,44 @@ void process_image_callback(const sensor_msgs::Image img)
 
     int white_pixel = 255;
     bool ball_found = false;
-    int counter  = 0; 
-    
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
+    int location  = -1; 
+    ROS_INFO("Process Image callback function has been called");
+ 
+    //Loop through each pixel in the image and check if there's a bright white one
     for (int i=0; i<img.height*img.step; i++)
-       {
-	 
-         if ((img.data[i]== white_pixel) && (img.data [i+1]== white_pixel) && (img.data[i+2])== white_pixel)
+     {
+	
+    	if ((img.data[i]== 255) && (img.data [i+1]== 255) &&(img.data[i+2])== 255)
           {
-	    ball_found = true;
- 	    counter = i % img.step;
-	    if (counter < img.step/3)
-		drive_robot(1.0,1);
-	    else if (counter < (img.step/3*2))
-		drive_robot(1.0,0);
-	    else 
-		drive_robot(1.0,-1);
+	    	ROS_INFO("The white ball has been found.");
+		ball_found = true;
+            	break;
+	      }
+ 	    
 
-	   break;
-	  }
+		// Then, identify if this pixel falls in the left, mid, or right side of the image 
+		float left_location= img.step/3;
+		float right_location=2*img.step/3;
+		//
+		location =i%img.step;
+		// Depending on the white ball position, call the drive_bot function and pass velocities to it
+		if (ball_found==true) {
+ 			//ROS_INFO( "The rotot can see the white ball.");
+			if (location < left_location)
+				drive_robot(0.0,1.0);
+			else if (location < right_location)
+				drive_robot(0.0,-1.0);
+			else //move forward if the white ball is neither left nor right
+				drive_robot(1.0,0.0);
 
-	 if (ball_found == false)
-	    drive_robot( 0.0, 0);
+			  
+		   }
 
-       }
-    
+		else // Request a stop when there's no white ball seen by the camera
+		 drive_robot( 0.0, 0.0);
+    }
+       
+   
 }
 
 int main(int argc, char** argv)
